799e084cbd2d8f065304209af97e4c791bdadb01
Ammar Bitar <ammar.bitar@outlook.com>
2020-02-07 12:43:17 +0100

    Plotting / Functions
    Working plotting for Rastagrin and Rosenbrock; Added user input for some params (iterations, nb_particles, function/benchmark)

--- functions.py
+++ functions.py
@@ -13,8 +13,14 @@ def Rosenbrock(x):
 def Rosenbrock_plt(x, y):
     return (x**2 + 100 * (y - x**2)**2)
 
+#def Rastrigin(x):
+#    #print("Rastrigin: {}".format(((10 * 2) + sum(x[i]**2 - 10 * np.cos(2 * np.pi * x[i]) for i in range(2)))))
+#    #print("len x: {}".format(len(x)))
+#    return ((10 * len(x)) + sum(x[i]**2 - 10 * np.cos(2 * np.pi * x[i]) for i in range(len(x))))
+
 def Rastrigin(x):
-    return ((10 * 2) + sum(x[i]**2 - 10 * np.cos(2 * np.pi * x[i]) for i in range(2)))
+#    return ((10 * 2) + (x[0]**2 - 10 * np.cos(2 * np.pi * x[0]) + x[1]**2 - 10 * np.cos(2 * np.pi * x[1])))
+    return ((10 * 2) + sum(x[i]**2 - 10 * np.cos(2 * np.pi * x[i]) for i in range(len(x))))
 
 def Rastrigin_plt(x, y):
-    return ((10 * len(x)) + sum(x[i]**2 - 10 * np.cos(2 * np.pi * x[i]) for i in range(len(x))))
+    return ((10 * 2) + ((x**2 - 10 * np.cos(2 * np.pi * x)) + (y**2 - 10 * np.cos(2 * np.pi * y))))
